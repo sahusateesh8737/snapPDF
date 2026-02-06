eab3579784105a3c60b7c0ddd6dd22a7aeafd96a
sahusateesh8737 <95178698+sahusateesh8737@users.noreply.github.com>
2026-02-06 14:20:39 +0530

    added vercel web analytics

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
         >
           {children}
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
